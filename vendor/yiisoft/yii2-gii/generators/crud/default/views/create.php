6d590c66639c36638c325dbdc2332604f96db6cf
liujiaming <22895327@qq.com>
2015-08-08 12:37:44 +0800

    vendor提交

--- vendor/yiisoft/yii2-gii/generators/crud/default/views/create.php
+++ vendor/yiisoft/yii2-gii/generators/crud/default/views/create.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 use yii\helpers\Inflector;
@@ -32,38 +31,3 @@ $this->params['breadcrumbs'][] = $this->title;
     ]) ?>
 
 </div>
-=======
-<?php
-
-use yii\helpers\Inflector;
-use yii\helpers\StringHelper;
-
-/* @var $this yii\web\View */
-/* @var $generator yii\gii\generators\crud\Generator */
-
-echo "<?php\n";
-?>
-namespace backend\controllers;
-use app\models\tables;
-use yii\helpers\Html;
-
-
-/* @var $this yii\web\View */
-/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
-
-$this->title = <?= $generator->generateString(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?> ;
-$title = Tables::find()->where(['tablename'=>$this->title])->one()['Ctablename'];
-$this->title = '添加'.$title;
-$this->params['breadcrumbs'][] = ['label' => $title, 'url' => ['<?= $generator->classString('{modelClass} ', ['modelClass' => StringHelper::basename($generator->modelClass)])?>index']];
-$this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-create">
-
-    <h1><?= "<?= " ?>Html::encode($this->title) ?></h1>
-
-    <?= "<?= " ?>$this->render('<?= $generator->classString('{modelClass} ', ['modelClass' => StringHelper::basename($generator->modelClass)])?>_form', [
-        'model' => $model,
-    ]) ?>
-
-</div>
->>>>>>> 943a37f9d0a01281ec6a4ae9c9db8b7fd4c69919
--- vendor/yiisoft/yii2-gii/generators/crud/form.php
+++ vendor/yiisoft/yii2-gii/generators/crud/form.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /* @var $this yii\web\View */
 /* @var $form yii\widgets\ActiveForm */
@@ -15,21 +14,3 @@ echo $form->field($generator, 'indexWidgetType')->dropDownList([
 ]);
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
-=======
-<?php
-/* @var $this yii\web\View */
-/* @var $form yii\widgets\ActiveForm */
-/* @var $generator yii\gii\generators\crud\Generator */
-
-echo $form->field($generator, 'modelClass')->label('模型名称，需要写全地址，例：app\models\模型名称');
-echo $form->field($generator, 'searchModelClass')->label('模型搜索类，例：frontend\models\模型名称Search');
-echo $form->field($generator, 'controllerClass')->label('控制器类，例：frontend\controllers\模型名称Controller');
-echo $form->field($generator, 'viewPath');
-echo $form->field($generator, 'baseControllerClass');
-echo $form->field($generator, 'indexWidgetType')->dropDownList([
-    'grid' => 'GridView',
-    'list' => 'ListView',
-]);
-echo $form->field($generator, 'enableI18N')->checkbox();
-echo $form->field($generator, 'messageCategory');
->>>>>>> 943a37f9d0a01281ec6a4ae9c9db8b7fd4c69919
