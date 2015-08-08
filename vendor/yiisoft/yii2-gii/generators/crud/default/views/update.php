d3e9f43387e65ff37f0abdd5dae13fc9be93995a
wubaiqing <wubaiqing@vip.qq.com>
2015-08-08 15:19:29 +0800

    提交代码

--- vendor/yiisoft/yii2-gii/generators/crud/default/views/update.php
+++ vendor/yiisoft/yii2-gii/generators/crud/default/views/update.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 use yii\helpers\Inflector;
@@ -33,39 +32,3 @@ $this->params['breadcrumbs'][] = <?= $generator->generateString('更新') ?>;
     ]) ?>
 
 </div>
-=======
-<?php
-
-use yii\helpers\Inflector;
-use yii\helpers\StringHelper;
-
-/* @var $this yii\web\View */
-/* @var $generator yii\gii\generators\crud\Generator */
-
-$urlParams = $generator->generateUrlParams();
-
-echo "<?php\n";
-?>
-namespace backend\controllers;
-use app\models\tables;
-use yii\helpers\Html;
-
-/* @var $this yii\web\View */
-/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
-
-$this->title = <?= $generator->generateString(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?> ;
-$this->title = Tables::find()->where(['tablename'=>$this->title])->one()['Ctablename'];
-$this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['<?= $generator->classString('{modelClass} ', ['modelClass' => StringHelper::basename($generator->modelClass)])?>index']];
-$this->params['breadcrumbs'][] = ['label' => $model-><?= $generator->getNameAttribute() ?>, 'url' => ['<?= $generator->classString('{modelClass} ', ['modelClass' => StringHelper::basename($generator->modelClass)])?>view', <?= $urlParams ?>]];
-$this->params['breadcrumbs'][] = <?= $generator->generateString('更新') ?>;
-?>
-<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-update">
-
-    <h1><?= "<?= " ?>Html::encode($this->title) ?></h1>
-
-    <?= "<?= " ?>$this->render('<?= $generator->classString('{modelClass} ', ['modelClass' => StringHelper::basename($generator->modelClass)])?>_form', [
-        'model' => $model,
-    ]) ?>
-
-</div>
->>>>>>> 943a37f9d0a01281ec6a4ae9c9db8b7fd4c69919
