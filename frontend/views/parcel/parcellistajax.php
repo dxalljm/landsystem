a5b0b9bce0cef194a568e59c7b2cc7b423d42e7c
liujiaming <22895327@qq.com>
2015-09-04 11:59:29 +0800

    修改宗地

---
+++ frontend/views/parcel/parcellistajax.php
@@ -0,0 +1,34 @@
+<?php
+namespace backend\controllers;
+use app\models\tables;
+use yii\helpers\Html;
+use yii\grid\GridView;
+use app\models\Parcel;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\parcelSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = '请选择宗地'
+?>
+<div class="parcel-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <table class="table table-striped table-bordered table-hover table-condensed">
+    
+    	<tr>
+    		<td align='center'>租赁面积（宗地）</td>
+    	</tr>
+    	<tr>
+    		<td align='center'><?= Html::textInput('parcellist','',['id'=>'parcellist'])?></td>
+
+    	</tr>
+    	<tr>
+    		<td align='center'><?php foreach($zdarea as $value) {echo html::button($value,['onclick'=>'toParcellist']);};?></td>
+
+    	</tr>
+    </table>
+
+</div>
