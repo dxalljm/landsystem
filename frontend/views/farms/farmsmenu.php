d45b5968f27b597f9a6820d25a3d8d7081b9c378
liujiaming <22895327@qq.com>
2015-09-04 12:03:46 +0800

    修改农场菜单农场信息

--- frontend/views/farms/farmsmenu.php
+++ frontend/views/farms/farmsmenu.php
@@ -7,25 +7,25 @@ use app\models\Theyear;
 use yii\widgets\ActiveFormrdiv;
 use yii\helpers\ArrayHelper;
 use app\models\Farms;
+use app\models\Cooperative;
 
 
 ?>
 
 <div class="farms-menu">
 <?php $form = ActiveFormrdiv::begin(); ?>
-<br><br><br>
+
 	<table class="table table-striped table-bordered table-hover table-condensed">
       <tr>
         <td width="128" align="right">管理区：</td>
-        <td width="193" align="left">&nbsp;
-        <?= html::dropDownList('managementarea',$areaid,ArrayHelper::map(ManagementArea::find()->all(),'id','areaname'),['id'=>'managementarea'])?>           </td>
+        <td width="193" align="left"><?= ManagementArea::find()->where(['id'=>$farm->management_area])->one()['areaname']?></td>
         <td width="92" align="right">农场名称：</td>
-        <td width="278" align="left">&nbsp;<?= html::dropDownList('farmname',$farm->id,ArrayHelper::map(Farms::find()->where(['management_area'=>$areaid])->all(),'id','farmname'),['id'=>'farmname','prompt'=>'请选择'])?>        </td>
+        <td width="278" align="left"><?= $farm->farmname?></td>
       </tr>
       <tr>
         <td align="right">合作社</td>
         <td align="left">&nbsp;
-          <?= $farm->cooperative_id;?></td>
+          <?= Cooperative::find()->where(['id'=>$farm->cooperative_id])->one()['cooperativename']?></td>
         <td align="right">审批年度：</td>
         <td align="left">&nbsp;
           <?= $farm->spyear;?></td>
@@ -57,7 +57,7 @@ use app\models\Farms;
   <tr><?php //Html::submitButton('',['style'=>'background:url("images/User With Frame.png") no-repeat; width:150px;height:150px;',])
         
         ?>
-    <td width="20%" align="center"><?=  Html::a(html::img('images/User With Frame.png',['width'=>'150']),'index.php?r=farmer/farmercreate&id='.$farm->id, [
+    <td width="20%" align="center"><?=  Html::a(html::img('images/User With Frame.png',['width'=>'100']),'index.php?r=farmer/farmercreate&id='.$farm->id, [
             			'id' => 'farmerview',
             			'title' => '承包信息',
             			'target' => '_blank',
@@ -66,7 +66,7 @@ use app\models\Farms;
             			//'onclick' => "javascript:window.open('".yii::$app->urlManager->createUrl(['/farmer/farmercreate','id'=>$farm->id])."','','width=1200,height=1000,top=150,left=180, toolbar=no, status=no, menubar=no, resizable=no, scrollbars=yes');return false;",
             			//'onclick' => 'farmercreate($("#farmname").val())',
              	]);?></td>
-    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerzr.png',['width'=>'150']),'#', [
+    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerzr.png',['width'=>'100']),'#', [
             			'id' => 'farmerview',
             			'title' => '转让信息',
             			'target' => '_blank',
@@ -74,7 +74,7 @@ use app\models\Farms;
             			'data-target' => '#leaseindex-modal',
             			'onclick' => 'leaseindex($("#farmname").val())',
              	]);?></td>
-    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerzp.png',['width'=>'150']),'index.php?r=lease/leaseindex&farms_id='.$farm->id, [
+    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerzp.png',['width'=>'100']),'index.php?r=lease/leaseindex&farms_id='.$farm->id, [
             			'id' => 'leaseview',
             			'title' => '租赁信息',
             			'target' => '_blank',
@@ -82,17 +82,17 @@ use app\models\Farms;
             			'data-target' => '#leaseindex-modal',
             			'onclick' => 'leaseindex($("#farmname").val())', */
              	]);?></td>
-    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerdk.png',['width'=>'150']),'#', [
+    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerdk.png',['width'=>'100']),'#', [
             			'id' => 'farmerview',
             			'title' => '抵押贷款信息',
             			'target' => '_blank',
             			'onclick' => "javascript:window.open('".yii::$app->urlManager->createUrl(['/lease/leaseindex','id'=>$farm->id,'year'=>$year])."','','width=1200,height=600,top=50,left=80, toolbar=no, status=no, menubar=no, resizable=no, scrollbars=yes');return false;",
              	]);?></td>
-    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerjf.png',['width'=>'150']),'#', [
+    <td width="20%" align="center"><?=  Html::a(html::img('images/farmerjf.png',['width'=>'100']),'index.php?r=dispute/disputeindex&farms_id='.$farm->id, [
             			'id' => 'farmerview',
             			'title' => '承包经营权纠纷',
             			'target' => '_blank',
-            			'onclick' => "javascript:window.open('".yii::$app->urlManager->createUrl(['/farmer/farmerview','id'=>$farm->id])."','','width=1200,height=600,top=50,left=80, toolbar=no, status=no, menubar=no, resizable=no, scrollbars=yes');return false;",
+            			//'onclick' => "javascript:window.open('".yii::$app->urlManager->createUrl(['/dispute/disputeindex','farms_id'=>$farm->id])."','','width=1200,height=600,top=50,left=80, toolbar=no, status=no, menubar=no, resizable=no, scrollbars=yes');return false;",
              	]);?></td>
   </tr>
   </table>
@@ -128,7 +128,7 @@ use app\models\Farms;
             			'target' => '_blank',
             			
              	]);?></td>
-    <td width="20%" align="center"><?=  Html::a('种值结构','index.php?r=plant/plantindex&farms_id='.$farm->id, [
+    <td width="20%" align="center"><?=  Html::a('种值结构','index.php?r=plantingstructure/plantingstructureindex&farms_id='.$farm->id, [
             			'id' => 'plantindex',
             			'title' => '种值结构',
             			'target' => '_blank',
