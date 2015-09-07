7136a2d7aa8981044f10414fbf83fbda06d32082
dxalljm <22895327@qq.com>
2015-09-07 19:22:54 +0800

    修改首页样式

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,12 +1,45 @@
 <?php
 /* @var $this yii\web\View */
+namespace frontend\controllers;
+use Yii;
 use yii\helpers\Html;
-$this->title = 'My Yii Application';
+use app\models\Farms;
+use app\models\User;
+use app\models\Department;
+$this->title = '岭南管委会';
 ?>
 <div class="site-index">
 
-
         <h1>欢迎您 <?= yii::$app->user->identity->username?></h1>
+        <?php 
+        $dep_id = User::findByUsername(yii::$app->user->identity->username)['department_id'];
+        $departmentData = Department::find()->where(['id'=>$dep_id])->one();
+
+        $whereArray = explode(',', $departmentData['membership']);
+
+        $farms = Farms::find()->where(['management_area'=>$whereArray])->all();
+        var_dump($farms);
+//         	$sumMeasure = 0;
+//         	foreach ($farms as $value) {
+//         		if()
+//         		$sumMeasure += $value['measure'];
+//         	}
+        ?>
+<!-- Apply any bg-* class to to the info-box to color it -->
+<div class="info-box bg-blue">
+  <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
+  <div class="info-box-content">
+    <span class="info-box-text">您所管辖的农场</span>
+    <span class="info-box-number"></span>
+    <!-- The progress section is optional -->
+    <div class="progress">
+      <div class="progress-bar" style="width: 70%"></div>
+    </div>
+    <span class="progress-description">
+      70% Increase in 30 Days
+    </span>
+  </div><!-- /.info-box-content -->
+</div><!-- /.info-box -->
 <br><br><br>
 <?php $action = Yii::$app->controller->action->id;?>
     <div class="body-content">
