dae723ccb6db759e7c9e315d97faccdd840db645
吴佰清 <wubaiqing@wubaiqingdeMacBook-Pro.local>
2015-09-10 16:01:50 +0800

    修改顶部导航

--- frontend/views/shares/header.php
+++ frontend/views/shares/header.php
@@ -11,7 +11,13 @@ use yii\helpers\Url;
     </a>
 
     <nav class="navbar navbar-static-top" role="navigation">
-        <div class="navbar-custom-menu">
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </a>
+        <div class="">
             <ul class="nav navbar-nav">
                 <li >
                     <a href="<?= Url::to('index.php?r=site/index')?>">首页</a>
