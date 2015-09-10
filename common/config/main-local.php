ebb17e60794f8016ebbdfb01909c48677d4e2c3d
吴佰清 <wubaiqing@wubaiqingdeMacBook-Pro.local>
2015-09-10 11:47:52 +0800

    修改配置文件‘

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -11,9 +11,9 @@ return [
 //         ],
     	'db' => [
     			'class' => 'yii\db\Connection',
-    			'dsn' => 'mysql:host=feature.jios.org;port=33060;dbname=landsystem',
-    			'username' => 'landsystem',
-    			'password' => 'landsystem',
+    			'dsn' => 'mysql:host=localhost;port=3306;dbname=landsystem',
+    			'username' => 'root',
+    			'password' => '',
     			'charset' => 'utf8',
     			'tablePrefix' => 'land_',
     	],
