c01fa4b884cf18e9c410e45e973b467d3f8fd13f
wubaiqing <wubaiqing@vip.qq.com>
2015-09-23 20:23:58 +0800

    Added Console

---
+++ console/controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace console\controllers;
+
+use yii\console\Controller;
+
+class TestController extends Controller
+{
+  public function actionIndex()
+  {
+    echo '123';
+
+  }
+
+}
