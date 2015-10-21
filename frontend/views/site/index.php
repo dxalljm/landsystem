8570c38f3975c632c481d89ff5fdb0bc7850b07d
wubaiqing <wubaiqing@vip.qq.com>
2015-10-21 14:29:56 +0800

    修改农场统计

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -28,9 +28,10 @@ $this->title = '岭南管委会';
     <div class="col-lg-3 col-xs-6">
       <!-- small box -->
       <div class="small-box bg-aqua">
+        <div id="farmer" style="min-width: 262px; height: 150px; margin: 0 auto"></div>
         <script type="text/javascript">
           $(function () {
-        	    $('#container').highcharts({
+        	    $('#farmer').highcharts({
         	        chart: {
         	            plotBackgroundColor: null,
         	            plotBorderWidth: null,
