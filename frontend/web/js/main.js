e91c14dd4f3721cf8fae5e987acf9f784e8c6421
wubaiqing <wubaiqing@vip.qq.com>
2015-10-23 10:38:20 +0800

    修改主题

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -4,145 +4,6 @@ if ($.fn.modal !== undefined) {
     $.fn.modal.Constructor.DEFAULTS.backdrop = 'static';
 }
 
-
-
-
-function farmercontract(key) {
-    // $('#createassign').click(function() {
-    // alert(key);
-    $.get(
-        'index.php',         
-        {
-            r: 'farmer/farmercontract',
-            id: key,
-
-        },
-        function (data) {
-            $('.modal-body').html(data);
-
-        }  
-    );
-    // });
-}
-function farmercreate(key) {
-    // $('#createassign').click(function() {
-    // alert(key);
-    $.get(
-        'index.php',         
-        {
-            r: 'farmer/farmercreate',
-            id: key,
-        },
-        function (data) {
-            $('.modal-body').html(data);
-
-        }  
-    );
-    // });
-}
-
-function leaseindex(farmsid) {
-    $.get(
-        'index.php',         
-        {
-            r: 'lease/leaseindex',
-            id: farmsid,
-        },
-        function (data) {
-            $('.modal-body').html(data);
-
-        }  
-    );
-    // });
-}
-function collectioncreate(farmsid,cardid) {
-    $('#leasecreate-modal').modal("hide");
-    $.get(
-        'index.php',         
-        {
-            r: 'collection/collectioncreate',
-            farms_id: farmsid,
-            cardid:cardid,
-
-        },
-        function (data) {
-            $('.modal-body').html(data);
-
-        }  
-    );
-    // });
-}
-
-
-/**
- * 租凭事件
- *
- * @returns {{isCreateButton: boolean, create: Function, close: Function}}
- */
-var leaseEvent = function () {
-
-    return {
-
-        /**
-         * @var boolean 是否点击创建按钮
-         */
-        isCreateButton: false,
-
-        /**
-         * @var int 农场ID
-         */
-        farmsId: 0,
-
-        /**
-         * 点击添加租凭
-         * @param int farmsId 农场ID
-         * @param int theYear 当前选择的年份
-         */
-        create: function (farmsId, theYear) {
-
-            _this = this;
-
-            // 设置当前ID
-            this.farmsId = farmsId;
-
-            // 隐藏当前modal
-            $.get('index.php', {r: 'lease/leasecreate', id: farmsId}, function (data) {
-
-                // 点击创建按钮
-                _this.isCreateButton = true;
-
-                // 填充数据
-                $('.modal-body').html(data);
-
-            });
-
-        },
-
-        /**
-         * 关闭租凭
-         */
-        close: function () {
-
-            _this = this;
-
-            // 关闭modal点击
-            $('#close-leaseindex-modal').on("click", function () {
-
-                // 重新请求数据，渲染视图
-                if (_this.isCreateButton == true) {
-                    leaseindex(_this.farmsId);
-                } else {
-                    $('#leaseindex-modal').modal('hide');
-                }
-
-                // 取消创建按钮事件
-                _this.isCreateButton = false;
-
-            });
-        }
-    }
-}
-
 Highcharts.theme = {
     colors: ["#DDDF0D", "#7798BF", "#55BF3B", "#DF5353", "#aaeeee", "#ff0066", "#eeaaee",
         "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
@@ -396,6 +257,148 @@ Highcharts.theme = {
 // Apply the theme
 var highchartsOptions = Highcharts.setOptions(Highcharts.theme);
 
+
+
+function farmercontract(key) {
+    // $('#createassign').click(function() {
+    // alert(key);
+    $.get(
+        'index.php',         
+        {
+            r: 'farmer/farmercontract',
+            id: key,
+
+        },
+        function (data) {
+            $('.modal-body').html(data);
+
+        }  
+    );
+    // });
+}
+function farmercreate(key) {
+    // $('#createassign').click(function() {
+    // alert(key);
+    $.get(
+        'index.php',         
+        {
+            r: 'farmer/farmercreate',
+            id: key,
+        },
+        function (data) {
+            $('.modal-body').html(data);
+
+        }  
+    );
+    // });
+}
+
+function leaseindex(farmsid) {
+    $.get(
+        'index.php',         
+        {
+            r: 'lease/leaseindex',
+            id: farmsid,
+        },
+        function (data) {
+            $('.modal-body').html(data);
+
+        }  
+    );
+    // });
+}
+function collectioncreate(farmsid,cardid) {
+    $('#leasecreate-modal').modal("hide");
+    $.get(
+        'index.php',         
+        {
+            r: 'collection/collectioncreate',
+            farms_id: farmsid,
+            cardid:cardid,
+
+        },
+        function (data) {
+            $('.modal-body').html(data);
+
+        }  
+    );
+    // });
+}
+
+
+/**
+ * 租凭事件
+ *
+ * @returns {{isCreateButton: boolean, create: Function, close: Function}}
+ */
+var leaseEvent = function () {
+
+    return {
+
+        /**
+         * @var boolean 是否点击创建按钮
+         */
+        isCreateButton: false,
+
+        /**
+         * @var int 农场ID
+         */
+        farmsId: 0,
+
+        /**
+         * 点击添加租凭
+         * @param int farmsId 农场ID
+         * @param int theYear 当前选择的年份
+         */
+        create: function (farmsId, theYear) {
+
+            _this = this;
+
+            // 设置当前ID
+            this.farmsId = farmsId;
+
+            // 隐藏当前modal
+            $.get('index.php', {r: 'lease/leasecreate', id: farmsId}, function (data) {
+
+                // 点击创建按钮
+                _this.isCreateButton = true;
+
+                // 填充数据
+                $('.modal-body').html(data);
+
+            });
+
+        },
+
+        /**
+         * 关闭租凭
+         */
+        close: function () {
+
+            _this = this;
+
+            // 关闭modal点击
+            $('#close-leaseindex-modal').on("click", function () {
+
+                // 重新请求数据，渲染视图
+                if (_this.isCreateButton == true) {
+                    leaseindex(_this.farmsId);
+                } else {
+                    $('#leaseindex-modal').modal('hide');
+                }
+
+                // 取消创建按钮事件
+                _this.isCreateButton = false;
+
+            });
+        }
+    }
+}
+
+
+// Apply the theme
+var highchartsOptions = Highcharts.setOptions(Highcharts.theme);
+
 // 实例化租凭合同
 lease = leaseEvent();
 lease.close();
@@ -410,7 +413,6 @@ var statis = function () {
                     $(function () {
                         $('#statis-farms').highcharts({
                         	chart: {
-                        		 plotBackgroundColor: '#00C0EF',
                                  plotBorderWidth: null,
                                  plotShadow: false
                             },
@@ -455,7 +457,6 @@ var statis = function () {
                 	$(function () {
                         $('#statis-area').highcharts({
                         	chart: {
-                        		 plotBackgroundColor: '#00C0EF',
                                  plotBorderWidth: null,
                                  plotShadow: false
                             },
