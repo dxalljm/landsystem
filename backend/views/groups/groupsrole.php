652d49574a4f4e274f04c5435a96dc3639d2e5db
liujiaming <22895327@qq.com>
2015-11-17 21:05:58 +0800

    工作组分配权限页面（未完成）

---
+++ backend/views/groups/groupsrole.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveFormrdiv;
+use yii\helpers\ArrayHelper;
+use app\models\ManagementArea;
+/* @var $this yii\web\View */
+/* @var $model app\models\Groups */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="groups-form">
+
+    <?php $form = ActiveFormrdiv::begin(); ?>
+<table class="table table-bordered table-hover">
+		<tr>
+<td width=15% align='right'>分配管理区</td>
+<td align='left'><?= html::checkboxList('managementarea','',ArrayHelper::map(ManagementArea::find()->all(), 'id', 'areaname'),['class'=>'form-control']) ?></td>
+</tr>
+<tr>
+<td width=15% align='right'>用户组权限</td>
+<td align='left'></td>
+</tr>
+<tr>
+<td width=15% align='right'>用户组标识</td>
+<td align='left'></td>
+</tr>
+</table>
+    <div class="form-group">
+       
+    </div>
+
+    <?php ActiveFormrdiv::end(); ?>
+
+</div>
