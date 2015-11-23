b7d71781d8616cd4e9f8209864c1adce57a1517c
dxalljm <22895327@qq.com>
2015-11-23 14:04:32 +0800

    search

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -14,6 +14,9 @@ use yii\filters\VerbFilter;
 use yii\data\ActiveDataProvider;
 use app\models\Plant;
 use app\models\Plantingstructure;
+use app\models\Collection;
+use app\models\User;
+use app\models\Department;
 /**
  * BankAccountController implements the CRUD actions for BankAccount model.
  */
@@ -49,16 +52,52 @@ class SearchController extends Controller
     			'planting' => $planting,
     	]);
     }
+    //获取用户管理区信息
+    public function getUserManagementArea()
+    {
+    	$departmentid = User::find ()->where ( [
+    			'id' => \Yii::$app->getUser ()->id
+    	] )->one ()['department_id'];
+    	$departmentData = Department::find ()->where ( [
+    			'id' => $departmentid
+    	] )->one ();
+    	$whereArray = explode ( ',', $departmentData ['membership'] );
+    	return $whereArray;
+    }
+    
+    //获取用户所在管理区的所有农场
+    public function getAllFarmsID()
+    {
+    	$farms = Farms::find()->where(['management_area'=>$this->getUserManagementArea()])->all();
+    	foreach ($farms as $value) {
+    		$result[] = $value['id'];
+    	}
+    	return $result;
+    }
     
     public function getPlantingstructure()
     {
-    	$planting = Plantingstructure::find()->all();
-    	foreach ($planting as $value) {
-    		$plantname = Plant::find()->where(['id'=>$value['plant_id']])->one()['cropname'];
-    		$result[$plantname]['area'][] = $value['area'];
-    		$result[$plantname]['goodseed_id'][] = $value['goodseed_id'];
+    	$planting = [];
+    	foreach($this->getAllFarmsID() as $value) {
+    		$p = Plantingstructure::find()->where(['farms_id'=>$value])->all();
+    		if(!empty($p))
+    			$planting[$value][] = $p;
+    	}
+// 		var_dump($planting[2][0]);exit;
+    	foreach ($planting as $key => $value) {
+    		foreach ($value as $val) {
+    			var_dump($val);exit;
+//     			$plantname = Plant::find()->where(['id'=>$val['plant_id']])->one()['cropname'];
+// 	    		$result[$plantname]['area'][] = $val['area'];
+// 	    		$result[$plantname]['goodseed_id'][] = $val['goodseed_id'];
+    		}
+    		
     	}
     	return $result;
     }
  
+    public function getCollection()
+    {
+    	$collection = Collection::find()->all(); 
+    } 
 }
